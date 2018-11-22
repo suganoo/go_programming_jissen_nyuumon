d856edcad96d730acee4572622a539b09b4b8a5a
suganoo <suganoo2002@gmail.com>
2018-11-22 19:06:22 +0900

    update

--- ch02/chitchat/utils.go
+++ ch02/chitchat/utils.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"encoding/json"
-	//"errors"
+	"errors"
 	"fmt"
 	//"./data"
-	//"html/template"
+	"html/template"
 	"log"
-	//"net/http"
+	"net/http"
 	"os"
-	//"strings"
+	"strings"
 )
 
 type Configuration struct {
@@ -46,6 +46,32 @@ func loadConfig() {
 	if err != nil {
 		log.Fatalln("Cannot get configuration from file", err)
 	}
+}
+
+func error_message(writer http.ResponseWriter, request *http.Request, msg string) {
+	url := []string{"/err?msg=", msg}
+	http.Redirect(writer, request, strings.Join(url, ""), 302)
+}
+
+func session(writer http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
+	cookie, err := request.Cookie("_cookie")
+	if err != nil {
+		sess = data.Session{Uuid: cookie.Value}
+		if ok, _ := sess.Check(); !ok {
+			err = errors.New("Invalid session")
+		}
+	}
+	return
+}
+
+func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
 }
 
 func version() string {
